92e427d8620581efa2b57af345427ee57798e61b
parislettau <55466163+parislettau@users.noreply.github.com>
2022-09-19 18:09:35 +1000

    Update home.php

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,6 +3,19 @@
 <?php snippet('nav') ?>
 <!-- content -->
 <main id="main" class="page-container">
+    <?php
+    exec('whoami', $output, $retval);
+    dump($output);
+    dump($retval);
+    ?>
+
+    <?php
+
+    exec('convert -v', $output);
+    dump($output);
+
+    ?>
+
     <div class="main-container">
         <?php snippet('home/grid') ?>
     </div>
