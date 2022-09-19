2702b44b8c192cb767e185d418d2beac6f98fb56
parislettau <55466163+parislettau@users.noreply.github.com>
2022-09-19 17:56:33 +1000

    Update config.php

--- site/config/config.php
+++ site/config/config.php
@@ -14,29 +14,29 @@ return [
     # https://getkirby.com/docs/reference/objects/cms/file/srcset
     'thumbs' => [
         'driver' => 'im',                                       //https://getkirby.com/docs/guide/troubleshooting/thumbnails
-        // 'bin' => 'convert',                        //https://getkirby.com/docs/guide/troubleshooting/thumbnails
-        'srcsets' => [                                          // https://getkirby.com/docs/cookbook/performance/responsive-images
-            'default' => [
-                '300w'  => ['width' => 300],
-                '600w'  => ['width' => 600],
-                '900w'  => ['width' => 900],
-                '1200w' => ['width' => 1200],
-                '1800w' => ['width' => 1800]
-            ],
-            'avif' => [
-                '300w'  => ['width' => 300, 'format' => 'avif'],
-                '600w'  => ['width' => 600, 'format' => 'avif'],
-                '900w'  => ['width' => 900, 'format' => 'avif'],
-                '1200w' => ['width' => 1200, 'format' => 'avif'],
-                '1800w' => ['width' => 1800, 'format' => 'avif']
-            ],
-            'webp' => [
-                '300w'  => ['width' => 300, 'format' => 'webp'],
-                '600w'  => ['width' => 600, 'format' => 'webp'],
-                '900w'  => ['width' => 900, 'format' => 'webp'],
-                '1200w' => ['width' => 1200, 'format' => 'webp'],
-                '1800w' => ['width' => 1800, 'format' => 'webp']
-            ],
-        ]
+        'bin' => '/usr/local/bin/convert',                       //https://getkirby.com/docs/guide/troubleshooting/thumbnails
+        // 'srcsets' => [                                          // https://getkirby.com/docs/cookbook/performance/responsive-images
+        //     'default' => [
+        //         '300w'  => ['width' => 300],
+        //         '600w'  => ['width' => 600],
+        //         '900w'  => ['width' => 900],
+        //         '1200w' => ['width' => 1200],
+        //         '1800w' => ['width' => 1800]
+        //     ],
+        //     'avif' => [
+        //         '300w'  => ['width' => 300, 'format' => 'avif'],
+        //         '600w'  => ['width' => 600, 'format' => 'avif'],
+        //         '900w'  => ['width' => 900, 'format' => 'avif'],
+        //         '1200w' => ['width' => 1200, 'format' => 'avif'],
+        //         '1800w' => ['width' => 1800, 'format' => 'avif']
+        //     ],
+        //     'webp' => [
+        //         '300w'  => ['width' => 300, 'format' => 'webp'],
+        //         '600w'  => ['width' => 600, 'format' => 'webp'],
+        //         '900w'  => ['width' => 900, 'format' => 'webp'],
+        //         '1200w' => ['width' => 1200, 'format' => 'webp'],
+        //         '1800w' => ['width' => 1800, 'format' => 'webp']
+        //     ],
+        // ]
     ]
 ];
