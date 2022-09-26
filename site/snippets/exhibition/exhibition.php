8bb4cc1337320a417a41686d61b5192ec15fa5cf
Paris Lettau <55466163+parislettau@users.noreply.github.com>
2022-09-26 22:27:54 +1000

    Update exhibition.php

--- site/snippets/exhibition/exhibition.php
+++ site/snippets/exhibition/exhibition.php
@@ -7,34 +7,9 @@
     }
     ?>
     <img src="<?= $image->placeholderUri() ?>" data-src="<?= $image->url() ?>" data-lazyload alt="<?= $image->alt() ?>" />
-    <img src="<?= $image->url() ?>" alt="<?= $image->alt() ?>" />
-
 </div>
-<?php
-if ($image = $page->image()) :
-    echo $image->thumb([
-        'width'   => 300,
-        'height'  => 200,
-        'quality' => 80,
-    ])->html();
-endif;
-
-if ($image = $page->image()) :
-    echo $image->thumb([
-        'width'   => 300,
-        'height'  => 200,
-        'quality' => 80,
-        'format'  => 'webp',
-    ])->html();
-endif;
-
-
-?>
-
 
 <div class="main-exhibition-text-container">
-    <br><br>
-    <strong>Image URL: <br><br><?= $image->url() ?></strong><br><br>
     <?= $page->title() ?><br>
     <?= $page->startDate()->toDate("d.m.Y") ?><?php if ($page->startDate()->isNotEmpty() && $page->endDate()->isNotEmpty()) : ?>—<?php endif ?><?= $page->endDate()->toDate("d.m.Y") ?><br><br>
     <?= $page->text()->kt() ?>
