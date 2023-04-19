dec8822d46631710cb629022ee854cef4a59259a
parislettau <55466163+parislettau@users.noreply.github.com>
2023-04-19 11:04:42 +1000

    Update config.php

--- site/config/config.php
+++ site/config/config.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    'debug' => true
+    'debug' => true, // turn to false once finished
+    'url' => 'https://trocadero-trocaderoprojects.9jgqd6.easypanel.host/',
+    'panel' => [
+        'install' => true
+    ]
 ];
-
-// turn to false once finished
